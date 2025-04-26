2d7ed5aff37680d670d0e74729f3c26c9739b62b
3kanAlpha <39447635+3kanAlpha@users.noreply.github.com>
2025-04-26 23:42:27 +0900

    fix: 月の表示がずれているバグを修正

--- src/utils/date-util.ts
+++ src/utils/date-util.ts
@@ -1,7 +1,7 @@
 export function formatJoinedDate(timestamp: string): string {
   const d = new Date(timestamp);
   const y = d.getFullYear();
-  const month = d.getMonth();
+  const month = d.getMonth() + 1;
 
   return `${y}年${month}月からNestterを利用しています`;
 }
