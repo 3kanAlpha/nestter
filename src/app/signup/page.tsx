2b3025d5b60489b9defd347620f94b155a9d9f33
3kanAlpha <39447635+3kanAlpha@users.noreply.github.com>
2025-04-26 17:44:34 +0900

    fix: 既に登録済みのユーザーが新規登録ページに入らないように修正

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,14 @@
+import { redirect } from "next/navigation";
+import { auth } from "@/auth";
 import SignupForm from "@/components/signup/signup-form";
 
-export default function Signup() {
+export default async function Signup() {
+  const session = await auth();
+  // 既にアカウントの初期設定が終わっているなら、リダイレクトする
+  if (session?.user && session.user.screenName) {
+    redirect(`/user/${session.user.screenName}`)
+  }
+
   return (
     <div className="flex flex-col items-center py-4">
       <h2 className="mb-2">
