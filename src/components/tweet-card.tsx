19d22d94a249d18ce882cc7659fbbfe790a017c3
3kanAlpha <39447635+3kanAlpha@users.noreply.github.com>
2025-05-01 21:29:14 +0900

    change: リプライボタンのデザイン変更

--- src/components/tweet-card.tsx
+++ src/components/tweet-card.tsx
@@ -92,7 +92,7 @@ export default function TweetCard({ tweet, user, attachments, isRetweet = false,
               {/* リプライ */}
               <div className="flex flex-row items-center gap-1 w-6">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M7.49 12 3.74 8.248m0 0 3.75-3.75m-3.75 3.75h16.5V19.5" />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" />
                 </svg>
               </div>
               {/* リツイート */}
