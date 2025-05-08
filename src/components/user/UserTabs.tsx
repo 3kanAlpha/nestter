5bbe0be7e122246018c00a3981f10396d1e74457
3kanAlpha <39447635+3kanAlpha@users.noreply.github.com>
2025-05-08 14:10:45 +0900

    change: UserTabsのUI調整

--- src/components/user/UserTabs.tsx
+++ src/components/user/UserTabs.tsx
@@ -15,32 +15,34 @@ export default function UserTabs({ user, authUserId }: Props) {
   const [active, setActive] = useState(0);
 
   return (
-    <div className="tabs tabs-border">
-      <input type="radio" name="user_page_tabs" className="tab" aria-label="ツイート" onClick={()=>setActive(0)} defaultChecked />
-      <div className="tab-content">
-        { active === 0 && (
-          <Suspense>
-            <TweetList from={user.screenName} authUserId={authUserId} excludeReply />
-          </Suspense>
-        ) }
-      </div>
+    <div className="overflow-x-auto">
+      <div className="tabs tabs-border flex">
+        <input type="radio" name="user_page_tabs" className="tab flex-auto" aria-label="ツイート" onClick={()=>setActive(0)} defaultChecked />
+        <div className="tab-content">
+          { active === 0 && (
+            <Suspense>
+              <TweetList from={user.screenName} authUserId={authUserId} excludeReply />
+            </Suspense>
+          ) }
+        </div>
 
-      <input type="radio" name="user_page_tabs" className="tab" aria-label="ツイートと返信" onClick={()=>setActive(1)} />
-      <div className="tab-content">
-        { active === 1 && (
-          <Suspense>
-            <TweetList from={user.screenName} authUserId={authUserId} />
-          </Suspense>
-        ) }
-      </div>
+        <input type="radio" name="user_page_tabs" className="tab flex-auto" aria-label="ツイートと返信" onClick={()=>setActive(1)} />
+        <div className="tab-content">
+          { active === 1 && (
+            <Suspense>
+              <TweetList from={user.screenName} authUserId={authUserId} />
+            </Suspense>
+          ) }
+        </div>
 
-      <input type="radio" name="user_page_tabs" className="tab" aria-label="いいね" onClick={()=>setActive(2)} />
-      <div className="tab-content">
-        { active === 2 && (
-          <Suspense>
-            <LikedTweetList authUserId={authUserId} userId={user.id} />
-          </Suspense>
-        ) }
+        <input type="radio" name="user_page_tabs" className="tab flex-auto" aria-label="いいね" onClick={()=>setActive(2)} />
+        <div className="tab-content">
+          { active === 2 && (
+            <Suspense>
+              <LikedTweetList authUserId={authUserId} userId={user.id} />
+            </Suspense>
+          ) }
+        </div>
       </div>
     </div>
   )
